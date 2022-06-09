7f024e587ad2eb466da76b8eee4b5192e8d34a12
ChrisKim <1515748548@qq.com>
2022-06-09 20:03:17 +0800

    程序启动时输出OSS配置

--- upload_oss.py
+++ upload_oss.py
@@ -6,10 +6,15 @@ with open("oss_config.yaml", "r") as config_file:
     config = yaml.safe_load(config_file)
 
 access_key_id = config["ID"]
+print("[INFO] 读取AccessKey ID完成")
 access_key_secret = config["Secret"]
+print("[INFO] 读取AccessKey Secret完成")
 bucket_name = config["Bucket"]
+print(f"[INFO] 读取Bucket Name完成: {bucket_name}")
 endpoint = config["Endpoint"]
+print(f"[INFO] 读取Endpoint完成: {endpoint}")
 directory = config["Directory"]
+print(f"[INFO] 读取上传路径完成: {directory}")
 
 bucket = oss2.Bucket(oss2.Auth(access_key_id, access_key_secret), endpoint, bucket_name)
 
