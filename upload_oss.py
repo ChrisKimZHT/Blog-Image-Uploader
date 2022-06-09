19dd2063c5cd5b16a35c5909d90baeae938dc90d
ChrisKim <1515748548@qq.com>
2022-06-09 19:53:15 +0800

    OSS上传修改

--- upload_oss.py
+++ upload_oss.py
@@ -9,20 +9,15 @@ access_key_id = config["ID"]
 access_key_secret = config["Secret"]
 bucket_name = config["Bucket"]
 endpoint = config["Endpoint"]
+directory = config["Directory"]
 
 bucket = oss2.Bucket(oss2.Auth(access_key_id, access_key_secret), endpoint, bucket_name)
 
 
-def upload_file(local_file: str, remote_path: str) -> bool:
+def upload_file(local_file: str):
     """OSS 上传文件
 
     :param local_file: 本地文件的路径和文件名
-    :param remote_path: 上传的远程路径
-    :return: 是否上传成功
     """
-    try:
-        file_name = os.path.basename(local_file)
-        bucket.put_object_from_file(remote_path + file_name, local_file)
-        return True
-    except Exception:
-        return False
+    file_name = os.path.basename(local_file)
+    bucket.put_object_from_file(os.path.join(directory, file_name), local_file)
