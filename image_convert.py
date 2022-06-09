497828ab6d9840d114a3c6f09d4c4527f13bf102
ChrisKim <1515748548@qq.com>
2022-06-09 19:53:24 +0800

    图片转码修改

--- image_convert.py
+++ image_convert.py
@@ -2,17 +2,14 @@ import os
 from PIL import Image
 
 
-def to_webp(original_file: str, webp_path: str) -> bool:
+def to_webp(original_file: str, webp_path: str) -> str:
     """图片转 WebP
 
     :param original_file: 原图路径和文件名
     :param webp_path: 转换后图片路径
-    :return: 是否转换成功
+    :return: 转换后的图片路径
     """
-    try:
-        file_name = os.path.basename(original_file)
-        ori = Image.open(original_file)
-        ori.save(webp_path + file_name, "WEBP")
-        return True
-    except Exception:
-        return False
+    ori = Image.open(original_file)
+    path = os.path.join(webp_path, os.path.splitext(os.path.basename(original_file))[0] + ".webp")
+    ori.save(path, "WEBP")
+    return path
