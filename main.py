d66190651aeb4229070bb59ab972047e87be76ef
ChrisKim <1515748548@qq.com>
2022-06-12 12:24:38 +0800

    拆分程序逻辑，优化程序结构

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def load_id_config() -> None:
         return
 
 
-def increase_id() -> None:
+def increase_id() -> bool:
     """将图片序号+1并更新当前序号"""
     global id_config
     img_id = int(id_config["Img_ID"])
@@ -47,61 +47,154 @@ def increase_id() -> None:
         with open("id_config.yaml", "w") as temp_config_file:
             yaml.safe_dump(id_config, temp_config_file)
         log.info("ID配置更新成功")
+        return True
     except:
         log.error("ID配置更新失败")
+        return False
 
 
-def main() -> bool:
-    """程序主逻辑
+def main():
+    """主函数"""
+    while True:
+        # 选择功能
+        print("1. 普通上传\n"
+              "2. 封面上传\n"
+              "3. 覆盖上传\n"
+              "0. 退出\n"
+              "选择上传模式：", end="")
+        choose = input()
+        if choose == "0":
+            break
+        elif choose == "1":
+            while common_upload():
+                pass
+        elif choose == "2":
+            while cover_upload():
+                pass
+        elif choose == "3":
+            while overwrite_upload():
+                pass
+        else:
+            print("输入异常。请重新输入")
 
-    :return: 返回True即循环运行，False代表退出程序
+
+def check(input_dir: str) -> str:
+    """接受用户输入图片路径且检查合法性，并且进行转换
+
+    :param input_dir: 输入的路径
+    :return: 图片路径
     """
+    # 替换\为/
+    input_dir = re.compile(r"\\").sub("/", input_dir)
+    # 删除多余引号
+    input_dir = re.compile("\"").sub("", input_dir)
+    if os.path.exists(input_dir):
+        log.info("输入路径: " + input_dir)
+        return input_dir
+    return ""
+
+
+def copy(post_id: str, img_id: str, img_dir: str) -> str:
+    """复制图片到/Images/Original
+
+    :param post_id: 文章ID
+    :param img_id: 图片ID
+    :param img_dir: 图片目录
+    :return: 图片路径
+    """
+    try:
+        img_name = f"{post_id}-{img_id}{os.path.splitext(img_dir)[-1]}"
+        new_dir = os.path.join(orig_path, img_name)
+        shutil.copy(img_dir, new_dir)
+        log.info("图片已复制至: " + new_dir)
+        print("图片已复制至: " + new_dir)
+        return new_dir
+    except:
+        log.error("图片复制失败")
+        return ""
+
+
+def convert(img_dir: str) -> str:
+    """图片转码为WebP
+
+    :param img_dir: 原图路径
+    :return: 图片路径
+    """
+    try:
+        new_dir = to_webp(img_dir, webp_path)
+        log.info("图片已转码至: " + new_dir)
+        print("图片已转码至: " + new_dir)
+        return new_dir
+    except:
+        log.error("图片转码失败")
+        return ""
+
+
+def upload(img_dir: str) -> str:
+    """上传图片至OSS
+
+    :param img_dir: 原图路径
+    :return:
+    """
+    try:
+        link = upload_file(img_dir)
+        log.info(f"图片已上传至OSS成功: {link}")
+        print(f"图片已上传至OSS成功: {link}")
+        return link
+    except:
+        log.error("图片上传至OSS失败")
+        return ""
+
+
+def common_upload() -> bool:
+    """普通上传
+
+    :return: 返回真则继续循环，假停止循环
+    """
+    log.info("开始普通上传")
     # 输出信息
     print(f"上传已就绪: {id_config['Post_ID']}-{id_config['Img_ID']}")
     # 输入图片路径并检测合法性
-    while True:
-        print("输入图片路径: ", end="")
-        img_dir = input()
-        if img_dir == "q" or img_dir == "Q":
-            return False
-        # 替换\为/
-        img_dir = re.compile(r"\\").sub("/", img_dir)
-        # 删除多余引号
-        img_dir = re.compile("\"").sub("", img_dir)
-        if os.path.exists(img_dir):
-            break
-        log.warning("路径异常，请重新输入。")
+    print("输入图片路径: ", end="")
+    input_str = input()
+    if input_str == "q" or input_str == "Q":  # q键退出
+        return False
+    img_dir = check(input_str)
+    if img_dir == "":  # 异常退出
+        return False
     # 复制图片到/Images/Original
-    try:
-        img_name = f"{id_config['Post_ID']}-{id_config['Img_ID']}{os.path.splitext(img_dir)[-1]}"
-        shutil.copy(img_dir, os.path.join(orig_path, img_name))
-        print("图片已复制至: " + os.path.join(orig_path, img_name))
-        log.info("图片已复制至: " + os.path.join(orig_path, img_name))
-    except:
-        log.error("图片复制失败")
-        return True
+    ori_img_dir = copy(id_config["Post_ID"], id_config["Img_ID"], img_dir)
+    if ori_img_dir == "":  # 异常退出
+        return False
     # 图片转码为WebP
-    try:
-        new_dir = to_webp(os.path.join(orig_path, img_name), webp_path)
-        print("图片已转码至: " + new_dir)
-        img_name = os.path.basename(new_dir)
-    except:
-        log.error("图片转码失败")
-        return True
+    webp_img_dir = convert(ori_img_dir)
+    if webp_img_dir == "":  # 异常退出
+        return False
     # 图片上传OSS
-    try:
-        link = upload_file(os.path.join(webp_path, img_name))
-        print(f"图片已上传至OSS成功: {link}")
-    except:
-        log.error("图片上传至OSS失败")
-        return True
+    link = upload(webp_img_dir)
+    if link == "":  # 异常退出
+        return False
     # 图片序号+1
-    increase_id()
-    return True
+    return increase_id()
+
+
+def cover_upload() -> bool:
+    """封面上传
+
+    :return: 返回真则继续循环，假停止循环
+    """
+    log.info("开始封面上传")
+
+
+def overwrite_upload() -> bool:
+    """覆盖上传
+
+    :return: 返回真则继续循环，假停止循环
+    """
+    log.info("开始覆盖上传")
 
 
 if __name__ == "__main__":
     load_id_config()
-    while main():
-        log.info("主函数完成")
+    main()
     log.info("==========程序退出==========")
