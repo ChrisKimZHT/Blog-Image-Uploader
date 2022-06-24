87aa0dde5a2081315fcde8fd2272c8a297f16a7a
ChrisKim <1515748548@qq.com>
2022-06-24 12:25:47 +0800

    将图片路径并入配置文件

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 import yaml
 
+# OSS相关配置
 oss_config = {
     "ID": "",
     "Secret": "",
@@ -10,47 +11,49 @@ oss_config = {
     "Link": "",
 }
 
+# 图片命名的相关配置
 post_config = {
     "Post_ID": "",
     "Img_ID": "",
 }
 
+# 该程序的设置
+program_config = {
+    "Original_Path": "",
+    "WebP_Path": "",
+}
+
 
 def init_config() -> None:
     print("======OSS设置======")
-    print("输入AccessKey ID: ", end="")
-    oss_config["ID"] = input()
-    print("输入AccessKey Secret: ", end="")
-    oss_config["Secret"] = input()
-    print("输入Bucket名: ", end="")
-    oss_config["Bucket"] = input()
-    print("输入Endpoint(不包含Bucket名): ", end="")
-    oss_config["Endpoint"] = input()
-    print("输入上传路径(开头无/): ", end="")
-    oss_config["Directory"] = input()
-    print("输入外链链接(留空则自动拼接): ", end="")
-    oss_config["Link"] = input()
-    if oss_config["Link"] == "":
-        oss_config["Link"] = oss_config["Bucket"] + "." + oss_config["Endpoint"]
+    oss_config["ID"] = input("输入AccessKey ID: ")
+    oss_config["Secret"] = input("输入AccessKey Secret: ")
+    oss_config["Bucket"] = input("输入Bucket名: ")
+    oss_config["Endpoint"] = input("输入Endpoint(不包含Bucket名): ")
+    oss_config["Directory"] = input("输入上传路径(开头无/): ")
+    oss_config["Link"] = input("输入外链链接(留空则自动拼接): ") or (oss_config["Bucket"] + "." + oss_config["Endpoint"])
     print("======文章设置======")
-    print("输入文章ID: ", end="")
-    post_config["Post_ID"] = input()
-    print("输入图片ID: ", end="")
-    post_config["Img_ID"] = input()
+    post_config["Post_ID"] = input("输入文章ID: ")
+    post_config["Img_ID"] = input("输入图片ID: ")
+    print("======程序设置======")
+    program_config["Original_Path"] = input("输入原图储存路径(留空默认为Images/Original/): ") or "Images/Original/"
+    program_config["WebP_Path"] = input("输入WebP储存路径(留空默认为Images/WebP/): ") or "Images/WebP/"
 
 
 def load_config() -> None:
-    global oss_config, post_config
+    global oss_config, post_config, program_config
     with open("config.yaml", "r") as config_file:
         all_config = yaml.safe_load(config_file)
     oss_config = all_config["oss"]
     post_config = all_config["post"]
+    program_config = all_config["program"]
 
 
 def save_config() -> None:
     all_config = {
         "oss": oss_config,
         "post": post_config,
+        "program": program_config,
     }
     with open("config.yaml", "w") as config_file:
         yaml.safe_dump(all_config, config_file, sort_keys=False)
@@ -58,8 +61,7 @@ def save_config() -> None:
 
 def increase_id() -> None:
     global post_config
-    img_id = int(post_config["Img_ID"])
-    img_id += 1
+    img_id = int(post_config["Img_ID"]) + 1
     post_config["Img_ID"] = "{:02d}".format(img_id)
     save_config()
 
