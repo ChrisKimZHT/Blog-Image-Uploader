12d61a53e9297783aedb7ed95deb76b8f3367f5e
ChrisKim <1515748548@qq.com>
2022-06-24 12:52:25 +0800

    图片支持转换多种格式

--- convert.py
+++ convert.py
@@ -1,5 +1,9 @@
+import os.path
+
 from PIL import Image
 
 
-def to_webp(original_file: str, webp_path: str) -> None:
-    Image.open(original_file).save(webp_path, "WEBP")
+def image_convert(original_file: str, converted_file: str) -> None:
+    new_suffix = os.path.splitext(converted_file)[-1][1:]  # 新图片后缀（不带点）
+    img = Image.open(original_file)
+    img.save(converted_file, new_suffix)
